a08324f4a69f9535507893b7c5d59af25070ef59
p.sidorov <p.sidorov@navsy.ru>
2022-06-22 13:27:44 +0300

    basic layout for login and chat pages

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+  require('postcss-import'),
+  require('postcss-preset-env')({stage: 1}),
+  // require('postcss-nested'),
+  require('postcss-simple-vars'),
+  require('postcss-font-magician'),
+  require('cssnano'),
+  //   require('autoprefixer'),
+ 
+  ]
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+// src/index.js
+
+import {sum} from './modules/sum';
+import './styles.css'
+
+// const root = document.querySelector('#root');
+// root.textContent = sum(6, -1).toString();
