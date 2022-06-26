004ff0faedd5c2325c88e95a15d94be01fc33d88
p.sidorov <p.sidorov@navsy.ru>
2022-06-26 13:38:23 +0300

    env variables

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const app = express()
-
-app.use(express.static('dist'))
-
-app.listen(3002)
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../.env.production') });
+const express = require('express');
+const app = express();
+app.use(express.static('dist'));
+console.log(process.env.PORT);
+app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT} !`))
